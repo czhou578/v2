d225fdc81c83cd9ad60e1ef727c1d37ad76629c8
czhou578 <colizu2020@gmail.com>
2022-01-09 20:48:49 -0800

    finished headerbar layout

--- my-app/components/HeaderBar.tsx
+++ my-app/components/HeaderBar.tsx
@@ -4,7 +4,18 @@ import styles from '../styles/headerbar.module.css'
 const HeaderBar: FC = () => {
   return (
     <div className={styles.navbarWrapper}>
-      <ul className="taskbar">
+      <div className={styles.logo}>
+        <svg width="85" height="85" viewBox="0 0 126 105" fill="none" xmlns="https://www.w3.org/2000/svg">
+          <path d="M123 7H7V54.1446H123L7 98H123" stroke="url(#paint0_linear)" stroke-width="13"/>
+          <defs>
+          <linearGradient id="paint0_linear" x1="65" y1="7" x2="65" y2="175.843" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#39D4DE"/>
+          <stop offset="1" stop-color="#AA23B6"/>
+          </linearGradient>
+          </defs>
+        </svg>
+      </div>
+      <ul className={styles.taskbar}>
         <li className={styles.listItem}>
           <a href="#section-2">About Me</a>
         </li>
