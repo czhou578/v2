43e00dd7ab37035527d7b24d19c728f660c92bca
czhou578 <colizu2020@gmail.com>
2022-01-18 16:22:34 -0800

    set up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,34 @@
-const path = require('path')
-
-
 module.exports = {
+  entry: __dirname + "/../pages/api/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: ["ts-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx,", ".css", ".pdf"], 
+  },
+  target: "web",
+  devTool: "inline-source-map",
+  devServer: {
+    compress: true,
+    port: 3000,
+    historyApiFallback: true
+  }
 
 }
