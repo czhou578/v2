b3b5cdc23bebd39e63b9ddb6529700f9813eece7
czhou578 <colizu2020@gmail.com>
2022-01-19 17:22:27 -0800

    finished skillslist layout and picture

--- my-app/components/Aboutcard.tsx
+++ my-app/components/Aboutcard.tsx
@@ -1,14 +1,16 @@
 import { FC } from "react";
 import styles from "../styles/aboutcard.module.css"
+import Image from "next/image";
+import profilePic from '../public/images/70.jpg'
 
 
 const SkillsList: FC<{header?: string, skillsList: string[]}> = ({header, skillsList}) => {
   return (
     <div>
       <h3>{header}</h3>
-      <ul>
+      <ul className={styles.list}>
         {skillsList.map((element, key) => {
-          <li key={key}>{element}</li>
+          return <li key={key} className={styles.listElements}>{element}</li>
         })}
       </ul>
     </div>
@@ -23,6 +25,9 @@ const Aboutcard: FC = () => {
       </div>
       <div>
         <pre>This is colin speaking</pre>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src={profilePic} alt="Pic of weather" height={250} width={250} className={styles.img}/>
       </div>
       <div>
         <SkillsList header="Skills" skillsList={["JavaScript", "HTML", "CSS", "AWS", "React"]} />
