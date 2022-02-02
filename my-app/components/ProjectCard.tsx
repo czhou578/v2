2ab69df2d2fc8a956a3a0d8be6541829a2a1c6f2
czhou578 <colizu2020@gmail.com>
2022-02-02 11:31:19 -0800

    testing out basic eslint configuration

--- my-app/components/ProjectCard.tsx
+++ my-app/components/ProjectCard.tsx
@@ -1,12 +1,12 @@
-import { ImageProps } from "next/image";
-import Image from "next/image";
-import ProjectSkillsList from "./ProjectSkillsList";
+import { ImageProps } from 'next/image';
+import Image from 'next/image';
+import ProjectSkillsList from './ProjectSkillsList';
 
 
 const ProjectCard: React.FC<{img: ImageProps, list: JSX.Element}> = () => {
   return (
     <div>
-      <Image src={img} width={500} height={200} />
+      {/* <Image src={img} width={500} height={200} /> */}
     </div>
   )
 };
@@ -14,7 +14,7 @@ const ProjectCard: React.FC<{img: ImageProps, list: JSX.Element}> = () => {
 const ProjectCardContainer: React.FC = () => {
   return (
     <div>
-      <ProjectCard img={"../public/images/70.jpg"}/>
+      {/* <ProjectCard img={"../public/images/70.jpg"}/> */}
     </div>
   )
 }
