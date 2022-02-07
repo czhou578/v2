e9dee4ffd87b9586528cfe127e000bae37752872
czhou578 <colizu2020@gmail.com>
2022-02-07 10:45:44 -0800

    added in styles from example online

--- my-app/components/DesignsContainer.tsx
+++ my-app/components/DesignsContainer.tsx
@@ -3,12 +3,21 @@ import UIDesign1 from "../public/images/UIDesign1.png"
 import UIDesign2 from "../public/images/UIDesign2.png"
 import BCollege1 from "../public/images/BC1.png"
 import BCollege2 from "../public/images/BC2.png"
+import Image from "next/image";
+import styles from '../styles/designContainer.module.css'
 
+const DesignCard: React.FC<{img: StaticImageData}> = ({img}) => {
+  return (
+    <div className={styles.wrapper}>
+      <Image src={img} width={600} height={300} alt="Image" />
+    </div>
+  )
+}
 
 const DesignsContainer: React.FC = () => {
   return (
     <div>
-
+      <DesignCard img={UIDesign1}/>
     </div>
   )
 }
