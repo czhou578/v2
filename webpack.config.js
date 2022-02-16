ca64ea23d5462b25e02e926280994835f35b14c4
czhou578 <colizu2020@gmail.com>
2022-02-16 15:36:07 -0800

    comment out file

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const CopyPlugin = require("copy-webpack-plugin");
-const WriteFilePlugin = require('write-file-webpack-plugin');
+// const CopyPlugin = require("copy-webpack-plugin");
+// const WriteFilePlugin = require('write-file-webpack-plugin');
 
-module.exports = {
-  entry: __dirname + "/../pages/api/index.tsx",
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.tsx?$/,
-        use: ["ts-loader"]
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx,", ".css", ".js"], 
-  },
-  target: "web",
-  devTool: "inline-source-map",
-  devServer: {
-    compress: true,
-    port: 3000,
-    historyApiFallback: true
-  },
+// module.exports = {
+//   entry: __dirname + "/../pages/api/index.tsx",
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: [
+//           "style-loader",
+//           {
+//             loader: "css-loader",
+//             options: {
+//               modules: true
+//             }
+//           }
+//         ]
+//       },
+//       {
+//         test: /\.tsx?$/,
+//         use: ["ts-loader"]
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: [".tsx,", ".css", ".js"], 
+//   },
+//   target: "web",
+//   devTool: "inline-source-map",
+//   devServer: {
+//     compress: true,
+//     port: 3000,
+//     historyApiFallback: true
+//   },
 
-  output: {
-    filename: "[name].[contenthash].js",
-    publicPath: __dirname + "/dist",
-  },
+//   output: {
+//     filename: "[name].[contenthash].js",
+//     publicPath: __dirname + "/dist",
+//   },
 
-}
+// }
 
