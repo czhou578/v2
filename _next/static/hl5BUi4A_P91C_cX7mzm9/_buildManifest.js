0fb7b070c6254cad21dd1f1fa4821be4e3a30aab
czhou578 <42430035+czhou578@users.noreply.github.com>
2022-02-18 02:04:55 +0000

    Deploying to gh-pages from @ czhou578/v2@6293a345c61fb26a75cb501ee9635d7994452d9b 🚀

--- _next/static/hl5BUi4A_P91C_cX7mzm9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/573-a67d5cb62d4227ba.js","static/css/58fff143af853c92.css","static/chunks/pages/index-8da38ba476871a42.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/vx6tu_rJOMjcOnpfFtu_J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/573-a67d5cb62d4227ba.js","static/css/58fff143af853c92.css","static/chunks/pages/index-3dda5e9703a89c94.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
