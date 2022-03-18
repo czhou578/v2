1d9d06f76ac7a82f0b656f45671444572113e8e5
czhou578 <colizu2020@gmail.com>
2022-03-18 13:31:39 -0700

    npx pretty, responsive styling for project cards

--- components/Aboutcard.tsx
+++ components/Aboutcard.tsx
@@ -35,11 +35,11 @@ const Aboutcard: FC = () => {
             I have experience making cool designs for show. From websites to
             simple logo designs, I strive to bring some elements of the future
             into my creations. In addition, I have many years of experience
-            writing online, allowing me to share my ideas and thinking to millions
-            around the world. I am currently a student in college studying
-            Computer Science. My dream is to be able to help design and architect
-            platforms that would help problem solvers communicate their solutions
-            to a pressing problem to the world.
+            writing online, allowing me to share my ideas and thinking to
+            millions around the world. I am currently a student in college
+            studying Computer Science. My dream is to be able to help design and
+            architect platforms that would help problem solvers communicate
+            their solutions to a pressing problem to the world.
           </pre>
         </div>
         <div className={styles.imgContainer}>
