ca851233c4b33838db210289795e459b438d6717
czhou578 <42430035+czhou578@users.noreply.github.com>
2022-10-02 21:23:05 +0000

    Deploying to gh-pages from @ czhou578/v2@f18ffef76afedd95d546d45ca23903122f407803 🚀

---
+++ _next/static/l-PTPa0hTLJRNO_s_FLNY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/828-85fb76ebe8ced3d0.js","static/css/1049a3e214164c54.css","static/chunks/pages/index-170bf69cf0d5a236.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
