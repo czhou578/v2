3db5bf746c7fcc9259c10fdf83a2332df54b8803
czhou578 <colizu2020@gmail.com>
2022-12-01 10:28:46 -0600

    updated skills list

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,6 +93,7 @@ const Home: NextPage = () => {
                 "Webpack",
                 "AWS",
                 "SQL",
+                "MongoDB",
               ]}
               toolsList={[
                 "Googling",
